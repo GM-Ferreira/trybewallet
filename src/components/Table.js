decfaa144d07e8c8759edb23f3c8723615fdb358
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-10-03 16:04:31 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
-          <tr>
-            <th scope="col">Descrição</th>
-            <th scope="col">Tag</th>
-            <th scope="col">Método de pagamento</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Moeda</th>
-            <th scope="col">Câmbio utilizado</th>
-            <th scope="col">Valor convertido</th>
-            <th scope="col">Moeda de conversão</th>
-            <th scope="col">Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th scope="col">Descrição</th>
+              <th scope="col">Tag</th>
+              <th scope="col">Método de pagamento</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Moeda</th>
+              <th scope="col">Câmbio utilizado</th>
+              <th scope="col">Valor convertido</th>
+              <th scope="col">Moeda de conversão</th>
+              <th scope="col">Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                <td>
+                  { Math.round(expense.exchangeRates[expense.currency].ask
+                  * 100) / 100 }
+                </td>
+                <td>
+                  { Math.round(expense.exchangeRates[expense.currency].ask
+                  * expense.value * 100) / 100 }
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
