9ea559bcdf900d926da3c6e290ab6e10ca65ce70
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2022-10-05 19:26:31 -0300

    Correções de CSS página Wallet e Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,42 @@ import PropTypes from 'prop-types';
 class Header extends Component {
   render() {
     const { email, expenses } = this.props;
+    const profile = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={ 1.5 }
+        stroke="currentColor"
+        className="profile-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488
+          0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966
+          8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+    );
     return (
-      <div>
-        <div>
-          <span> Total de despesas: </span>
-          <span data-testid="total-field">
+      <div className="email-value-header">
+        <div className="total-fiel-general">
+          <span className="total-field-title"> Total de despesas: </span>
+          <span className="total-field-value" data-testid="total-field">
             { Number(expenses.map((e) => e.value
             * e.exchangeRates[e.currency].ask).reduce((pr, cur) => pr
             + cur, 0)).toFixed(2)}
           </span>
-          <span data-testid="header-currency-field">BRL</span>
+          <span
+            className="total-field-value"
+            data-testid="header-currency-field"
+          >
+            BRL
+          </span>
         </div>
-        <div data-testid="email-field">
+        <div className="email-field" data-testid="email-field">
+          { profile }
           { email }
         </div>
       </div>
--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -63,126 +63,143 @@ class WalletForm extends Component {
     const { currenciesList, editor, idToEdit } = this.props;
     const { valor, description, currencies, method, tag } = this.state;
     return (
-      <div>
-        <label htmlFor="valor">
-          Valor:
-          <input
-            type="text"
-            name="valor"
-            id="valor"
-            value={ valor }
-            data-testid="value-input"
-            onChange={ (e) => this.handleChange(e.target) }
-          />
-        </label>
-        <label htmlFor="description">
-          Descrição da despesa:
-          <input
-            type="text"
-            name="description"
-            id="description"
-            value={ description }
-            data-testid="description-input"
-            onChange={ (e) => this.handleChange(e.target) }
-          />
-        </label>
-        <label htmlFor="currencies">
-          Moeda:
-          <select
-            onChange={ (e) => this.handleChange(e.target) }
-            name="currencies"
-            id="currencies"
-            data-testid="currency-input"
-            value={ currencies }
-          >
-            {currenciesList.map((currency) => (
+      <div className="form-general">
+        <div className="form-general-no-button">
+
+          <label htmlFor="valor">
+            Valor:
+            <input
+              className="value-input"
+              type="text"
+              name="valor"
+              id="valor"
+              value={ valor }
+              data-testid="value-input"
+              onChange={ (e) => this.handleChange(e.target) }
+            />
+          </label>
+          <label htmlFor="description">
+            Descrição da despesa:
+            <input
+              className="description-input"
+              type="text"
+              name="description"
+              id="description"
+              value={ description }
+              data-testid="description-input"
+              onChange={ (e) => this.handleChange(e.target) }
+            />
+          </label>
+          <label htmlFor="currencies">
+            Moeda:
+            <select
+              className="currency-input"
+              onChange={ (e) => this.handleChange(e.target) }
+              name="currencies"
+              id="currencies"
+              data-testid="currency-input"
+              value={ currencies }
+            >
+              {currenciesList.map((currency) => (
+                <option
+                  key={ currency }
+                  value={ currency }
+                  name="currencies"
+                >
+                  { currency}
+
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="method">
+            Método de pagamento:
+            <select
+              className="method-input"
+              onChange={ (e) => this.handleChange(e.target) }
+              name="method"
+              id="method"
+              data-testid="method-input"
+              value={ method }
+            >
               <option
-                key={ currency }
-                value={ currency }
-                name="currencies"
+                value="Dinheiro"
+                name="method"
               >
-                { currency}
+                Dinheiro
+              </option>
+              <option
+                value="Cartão de crédito"
+                name="method"
+              >
+                Cartão de crédito
+              </option>
+              <option
+                value="Cartão de débito"
+                name="method"
+              >
+                Cartão de débito
+              </option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tipo de despesa:
+            <select
+              className="tag-input"
+              onChange={ (e) => this.handleChange(e.target) }
+              name="tag"
+              id="tag"
+              data-testid="tag-input"
+              value={ tag }
+            >
+              <option
+                value="Alimentação"
+              >
+                Alimentação
+              </option>
+              <option
+                value="Lazer"
+              >
+                Lazer
+              </option>
+              <option
+                value="Trabalho"
+              >
+                Trabalho
+              </option>
+              <option
+                value="Transporte"
+              >
+                Transporte
+              </option>
+              <option
+                value="Saúde"
+              >
+                Saúde
 
               </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="method">
-          Método de pagamento:
-          <select
-            onChange={ (e) => this.handleChange(e.target) }
-            name="method"
-            id="method"
-            data-testid="method-input"
-            value={ method }
-          >
-            <option
-              value="Dinheiro"
-              name="method"
-            >
-              Dinheiro
-            </option>
-            <option
-              value="Cartão de crédito"
-              name="method"
-            >
-              Cartão de crédito
-            </option>
-            <option
-              value="Cartão de débito"
-              name="method"
-            >
-              Cartão de débito
-            </option>
-          </select>
-        </label>
-        <label htmlFor="tag">
-          Tipo de despesa:
-          <select
-            onChange={ (e) => this.handleChange(e.target) }
-            name="tag"
-            id="tag"
-            data-testid="tag-input"
-            value={ tag }
-          >
-            <option
-              value="Alimentação"
-            >
-              Alimentação
-            </option>
-            <option
-              value="Lazer"
-            >
-              Lazer
-            </option>
-            <option
-              value="Trabalho"
-            >
-              Trabalho
-            </option>
-            <option
-              value="Transporte"
-            >
-              Transporte
-            </option>
-            <option
-              value="Saúde"
-            >
-              Saúde
-
-            </option>
-          </select>
-        </label>
+            </select>
+          </label>
+        </div>
         {editor
           ? (
             <button
+              className="form-button"
               onClick={ () => this.handleClickEdit(idToEdit) }
               type="button"
             >
               Editar despesa
             </button>
           )
-          : <button onClick={ this.handleClick } type="button">Adicionar despesa</button>}
+          : (
+            <button
+              className="form-button"
+              onClick={ this.handleClick }
+              type="button"
+            >
+              Adicionar despesa
+            </button>
+          )}
       </div>
     );
   }
